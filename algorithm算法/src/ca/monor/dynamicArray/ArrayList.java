3fd8181a7ac9c20bfbaafff76218dcc9f85cfe80
chongchen <chenchong.blog@gmail.com>
2019-09-09 16:09:02 -0400

    test

--- algorithm算法/src/ca/monor/dynamicArray/ArrayList.java
+++ algorithm算法/src/ca/monor/dynamicArray/ArrayList.java
@@ -1,22 +1,114 @@
 package ca.monor.dynamicArray;
 
 public class ArrayList<E> {
+    /**
+     * 元素的数量
+     */
     private int size;
+    /**
+     * 所有的元素
+     */
     private E[] elements;
 
-    private static final int DEFAULT_CAPACITY = 10;
-    private static final int ELEMENT_NOT_FOUND = -1;
+    private static final int DEFAULT_CAPACITY = 10; //给定数组的初始长度
+    private static final int ELEMENT_NOT_FOUND = -1; //元素未找到时返回 -1
 
-    public ArrayList(int capacity) {
+    public ArrayList(int capacity) {  // 有参构造，可以定义容量
+        // 长度小于默认长度则初始化数组长度为初始化长度
         capacity = (capacity < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : capacity;
+
+        //因爲 E 类型是Object 类的子类，可以无需判断，直接向下造型
         elements = (E[]) new Object[capacity];
     }
 
-    public ArrayList() {
+    /**
+     * 直接 return size 即为当前数组长度
+     *
+     * @return
+     */
+    public int size() {
+        return size;
+    }
 
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public ArrayList() {
+        this(DEFAULT_CAPACITY);  //用户未指定长度，则创建指定长度的数组
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    public void add(E element) {
+        add(size, element);
     }
 
     public void add(int index, E element) {
+        rangeCheckForAdd(index);
+        ensureCapacity(size + 1);
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = element;
+        size++;
+    }
 
+    public boolean contains(E element) {
+        return indexOf(element) != ELEMENT_NOT_FOUND;
+    }
+
+    public E get(int index) {
+        rangeCheck(index);
+        return elements[index];
+    }
+
+    public E set(int index, E element) {
+        rangeCheck(index);
+        E old = elements[index];
+        elements[index] = element;
+        return old;
+    }
+
+    public int indexOf(E element) {
+        if (element == null) {
+            for (int i = 0; i < size; i++) {
+                if (elements[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (element.equals(elements[i])) {
+                    return i;
+                }
+            }
+        }
+        return ELEMENT_NOT_FOUND;
+    }
+
+    private void ensureCapacity(int index) {
+
+    }
+
+    private void rangeCheck(int index) {
+        if (index < 0 || index >= size) {
+            outOfBounds(index);
+        }
+    }
+
+    private void rangeCheckForAdd(int index) {
+        if (index < 0 || index > size) {
+            outOfBounds(index);
+        }
+    }
+
+    private void outOfBounds(int index) {
+        throw new IndexOutOfBoundsException("Index: " + index + " size: " + size);
     }
 }
