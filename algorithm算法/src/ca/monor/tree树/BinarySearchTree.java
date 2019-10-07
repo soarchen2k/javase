cbbf9bcc945354c42a75eab7bea7bf821e47f859
soarchen2k <chenchong.blog@gmail.com>
2019-10-07 15:37:48 -0400

    MacBook

--- algorithm算法/src/ca/monor/tree树/BinarySearchTree.java
+++ algorithm算法/src/ca/monor/tree树/BinarySearchTree.java
@@ -33,6 +33,98 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
     public void add(E element) {
         elementNotNullCheck(element);
 
+        // add first node (root)
+        if (root == null) {
+            root = new Node<>(element, null);  //头节点的 parent 是 null
+            size++;
+            return;
+        }
+
+        // add to other nodes, first find the parent
+        Node<E> parent = root;
+        Node<E> node = root;
+        int elementComparator = 0;
+        do {
+            elementComparator = compare(element, node.element);
+            parent = node; //向左或向右之前，先保存父节点
+            if (elementComparator > 0) {  //要插入的节点的值比当前节点的值大
+                node = node.right;  // 则将 node 移动到当前 node 的右节点，成为新的要插入的节点的 parent
+            } else if (elementComparator < 0) {  //要插入的节点值比当前节点值小
+                node = node.left;  // 则将 node 移动到当前 node 的左节点
+            } else {  //如果要插入的值等于当前节点的值，就用 element 覆盖当前节点的 element
+                node.element = element;
+                return;
+            }
+        } while (node != null);  //当进行到空节点时，则比较结束
+
+        // 判断 element 将插入 parent 的哪个位置(left or right)
+        Node<E> newNode = new Node<>(element, parent);
+        if (elementComparator > 0) {  //新节点的值较大
+            parent.right = newNode;  //则做爲 parent 的 right
+        } else {
+            parent.left = newNode;  //否则将做爲 parent 的 left
+        }
+        size++;
+    }
+
+    public void remove(E element) {  // 删除某元素的 api
+        remove(node(element));  // 建立一个删除节点的 api，并调用
+    }
+
+    private void remove(Node<E> node) {  //删除某节点的 api
+        if (node == null) { //节点为空，则无法进行 remove 操作
+            return;
+        }
+        if (node.hasTwoChildren()) {  //节点的度为 2，需找出其后继结点
+            Node<E> s = successor(node);
+        }
+    }
+
+    private Node<E> successor(Node<E> node) {
+        if (node == null) {
+            return null;
+        }
+
+        //前驱结点在左子树当中 (right.left.left.left....)
+        Node<E> p = node.right;
+        if (p != null) {
+            while (p.left != null) {
+                p = p.left;
+            }
+            return p;
+        }
+
+        //从父节点，祖父节点中查找前驱结点
+        while (node.parent != null && node == node.parent.right) {
+            node = node.parent;
+        }
+        return node.parent;
+    }
+
+    private Node<E> node(E element) {  //查找元素所在的节点，因为是 BST，所有没有重复的节点
+        Node<E> node = root;
+        while (node != null) {
+            int elementComparator = compare(element, node.element);
+
+            if (elementComparator == 0) return node; //如果 node 的值等于 element，则返回 node
+            if (elementComparator < 0) { // 因为==0的时候已经返回，无需判断==0的情况
+                                         // 小于 0 说明 element 小，继续向左比较
+                node = node.left;
+            } else {
+                node = node.right;  // > 0 说明 element 较大，继续向右比较
+            }
+        }
+        return null;  //如果 while 循环内没有返回值，说明无此节点
+    }
+
+    private int compare(E element1, E element2) {
+        if (comparator != null) {  //如果创建的 BST 携带比较器，就使用 BST 自带的比较器进行比较
+            return comparator.compare(element1, element2);
+        }
+        // 因为 BST 必须是有序的，所以如果没有建立比较器，则强制要求当前比较的元素
+        // 的 class 必须实现 comparable 接口
+        return ((Comparable<E>) element1).compareTo(element2);
+
     }
 
     private void elementNotNullCheck(E element) {
