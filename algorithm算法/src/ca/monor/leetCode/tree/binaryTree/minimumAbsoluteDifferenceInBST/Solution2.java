f74d4ca1eef23a089e95bddaa30cf5df0d55dcf0
chongchen <chenchong.blog@gmail.com>
2019-10-25 23:34:23 -0400

    iMac

--- algorithm算法/src/ca/monor/leetCode/tree/binaryTree/minimumAbsoluteDifferenceInBST/Solution2.java
+++ algorithm算法/src/ca/monor/leetCode/tree/binaryTree/minimumAbsoluteDifferenceInBST/Solution2.java
@@ -1,13 +1,9 @@
-package ca.monor.leetCode.tree.binaryTree.validateBinarySearchTree;
+package ca.monor.leetCode.tree.binaryTree.minimumAbsoluteDifferenceInBST;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * 98. Validate Binary Search Tree
- * https://leetcode.com/problems/validate-binary-search-tree/
- */
-public class ValidateBinarySearchTree {
+public class Solution2 {
     class TreeNode {
         int val;
         TreeNode left;
@@ -18,19 +14,28 @@ public class ValidateBinarySearchTree {
         }
     }
 
-    public boolean isValidBST(TreeNode root) {
-        List<Integer> list1 = new ArrayList<>();
+    public int getMinimumDifference(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (hasNoChild(root)) {
+            return Math.abs(root.val);
+        }
 
+        List<Integer> list1 = new ArrayList<>();
+        int minimum = Integer.MAX_VALUE;
         //对树进行中序遍历，并把结果记录到 list 中
         inOrder(root, list1);
 
         //如果 list 中的前一个元素大于或等于下一个元素，则不是 BST
         for (int i = 0; i < list1.size() - 1; i++) {
-            if (list1.get(i) >= (list1.get(i + 1))) {
-                return false;
-            }
+            minimum = Math.min(minimum, Math.abs(list1.get(i) - list1.get(i + 1)));
         }
-        return true;
+        return minimum;
+    }
+
+    private boolean hasNoChild(TreeNode root) {
+        return root.left == null && root.right == null;
     }
 
     private void inOrder(TreeNode root, List<Integer> list1) {
