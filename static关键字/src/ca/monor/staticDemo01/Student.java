b415415eebed241d6038eecbbcf19467674b1887
chongchen <chenchong.blog@gmail.com>
2019-11-18 13:15:28 -0500

    iMac

--- static关键字/src/ca/monor/staticDemo01/Student.java
+++ static关键字/src/ca/monor/staticDemo01/Student.java
@@ -1,12 +1,22 @@
 package ca.monor.staticDemo01;
 
+/**
+ * 关键字 static，声明一个静态类、静态方法或静态属性
+ * 静态类：
+ * 静态方法：是属于类的方法，无法通过对象来访问，只能通过类名来访问静态方法
+ * 静态属性：属于类的属性，所有对象共享同一个 static 属性，每一个类对象都可以操作 static 属性，
+ * 也可以不建立对象，直接通过类来操作 static 属性
+ */
 public class Student {
     private String name;
     private int age;
-    private int sid;
-    public static int numberOfStudents = 0;
 
-    public Student(String name, int age) {
+    //为每个学生分配唯一的 SID
+    private int sid;
+    //学生的总数，通过学生的总数来分配学号
+    private static int numberOfStudents = 0;
+
+    Student(String name, int age) {
         this.name = name;
         this.age = age;
         this.sid = ++numberOfStudents;
@@ -14,6 +24,10 @@ public class Student {
 
     public void show() {
         System.out.println("Student name: " + name + ", age: " + age + ", sid: " + sid);
+    }
+
+    public static int getNumberOfStudents() {
+        return numberOfStudents;
     }
 
     public static void showNumber() {
--- static关键字/src/ca/monor/staticDemo01/TestStudent.java
+++ static关键字/src/ca/monor/staticDemo01/TestStudent.java
@@ -1,20 +1,22 @@
 package ca.monor.staticDemo01;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class TestStudent {
     public static void main(String[] args) {
-        Student student1 = new Student("Jamse1", 22);
-        Student student2 = new Student("Jamse2", 23);
-        Student student3 = new Student("Jamse3", 24);
-        Student student4 = new Student("Jamse4", 25);
-        System.out.println(Student.numberOfStudents);
         ArrayList<Student> students = new ArrayList<>();
-        students.add(student1);
-        students.add(student2);
-        students.add(student3);
-        students.add(student4);
+        students.add(new Student("Jamse1", 22));
+        students.add(new Student("Jamse2", 32));
+        students.add(new Student("Jamse3", 25));
+        students.add(new Student("Jamse4", 27));
 
+        System.out.println(Student.getNumberOfStudents());
+
+        for (Iterator<Student> iterator = students.iterator(); iterator.hasNext(); ) {
+            Student next = iterator.next();
+            System.out.println(next);
+        }
 
     }
 }
